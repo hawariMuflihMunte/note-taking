198e9772db064c568ca267cea5899c120e91782e
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-08-23 20:31:04 +0700

    Create: appData component

---
+++ src/components/form/appData.js
@@ -0,0 +1,20 @@
+import DOM from '../../classes/dom';
+
+export default function appData() {
+  const template = `
+    <article id="app__data">
+      <form id="app__data-form">
+        <input type="text" name="title" placeholder="Title" />
+        <textarea name="content" placeholder="Today is ...">
+        <button type="reset">Reset</button>
+        <button type="submit">Add</button>
+      </form>
+    </article></textarea></form></article>
+  `.trim();
+
+  const node = DOM.parser({
+    template
+  });
+
+  return node;
+}
